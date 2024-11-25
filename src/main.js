1edbfac98dc372bf7b6b0898f8ad6c30034f9e39
SonataGMX <sonata.velevko@geomatrix.lt>
2024-11-26 00:16:58 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const { currentUrl } = store.states;
 export default createEodash({
   id: "template-id",
   stacEndpoint:
-    "https://jorop.github.io/eodash-test/template_catalog/catalog.json",
+    "https://sonatagmx.github.io/eodash-test/template_catalog/catalog.json",
   brand: {
     name: "Dashboard",
     font: {
