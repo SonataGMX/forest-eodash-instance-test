a94d59a16cdfb2be06439fb757b2aea083765eb1
SonataGMX <sonata.velevko@geomatrix.lt>
2024-11-26 15:49:05 +0200

    Update main.js logo

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ export default createEodash({
       family: "Poppins",
       link: new URL("./assets/poppins.css", import.meta.url).href,
     },
-    logo: "./assets/geomatrix-logotipas.png",
+    logo: "/logo.png",
     errorMessage:
       "something went wrong, please contact demo@email.com if the issue persists",
     footerText: "eodash instance template",
