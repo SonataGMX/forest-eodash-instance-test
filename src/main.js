43f87ce381c0ddac94d0f10c27ff797fc867958f
SonataGMX <sonata.velevko@geomatrix.lt>
2024-11-26 10:55:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ export default createEodash({
         id: Symbol(),
         type: "internal",
         title: "Layer Control",
-        layout: { x: 0, y: 6, w: 2.5, h: 6 },
+        layout: { x: 0, y: 6, w: 3, h: 6 },
         widget: {
           name: "EodashLayerControl",
         },
